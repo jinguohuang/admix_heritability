68281290d22b8dc26e86ef7856aaac2e3b9c7a3f
Jinguo Huang <jinguohuang2018@gmail.com>
2020-10-28 22:13:04 -0400

    script for 11 demes simulation

---
+++ anc_estimate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+##For processing and ancestry estimation of msprime generated genotype data
+
+name=AA_100Mb_11deme
+
+echo "Change REF/ALT to A/T in the exported VCF file"
+# save header to a separate file
+head -n6 ${name}.vcf > header_${name}.txt
+#use awk to replace the ref/alt alleles with A/T, then concatenate with header 
+#also, add ID column to each variant
+cat header_${name}.txt <(cat ${name}.vcf \
+	| awk -v OFS="\t" 'NR>6 {$3=$1"_"$2"_A_T"; $4="A"; $5="T"; print ;}') > ${name}_AT.vcf
+
+echo "converting ${name} to PLINK format"
+plink \
+--vcf ${name}_AT.vcf \
+--make-bed \
+--out ${name}_AT
+
+echo "MAF filtering"
+plink \
+--bfile ${name}_AT \
+--maf 0.05 \
+--make-bed \
+--out ${name}_AT_maf0.05
+
+
+echo "LD pruning"
+plink \
+--bfile ${name}_AT_maf0.05 \
+--indep-pairwise 100 10 0.1 \
+--out ${name}_AT_maf0.05
+
+plink \
+--bfile ${name}_AT_maf0.05 \
+--extract ${name}_AT_maf0.05.prune.in \
+--make-bed \
+--out ${name}_AT_maf0.05_pruned
+
+# Run admixture and plot
+echo "run ancestry estimation"
+admixture --cv -j6 ${name}_AT_maf0.05_pruned.bed 2
+
+
+# Plot the result
+echo "plotting for the ancestry estimation"
+Rscript plot_admixture.R ${name}_AT_maf0.05_pruned.2.Q ${name}_AT_maf0.05_pruned.fam
+
+echo "Done ancestry estimation!"
---
+++ plot_admixture.R
@@ -0,0 +1,67 @@
+#!/usr/bin/env Rscript
+
+# for plotting structure of admixture results for each K
+
+#Usage:
+#-Place the following files in the same folder as this script
+# 	1) your plink binary files (.fam), 
+#	2) ADMIXTURE results file (.Q), 
+
+# load file
+args = commandArgs(trailingOnly=TRUE)
+Qfile<-args[1]  # load .Q file from ADMIXUTRE result
+famfile<-args[2] # load fam file from dataset
+
+# get filename and K 
+Name <- sub("\\_.*", '', Qfile)
+K <- as.numeric(sub('.*?\\.(.*?)\\.Q', '\\1', Qfile))
+
+require(reshape2)
+require(ggplot2)
+
+#load the .Q file
+qfile=read.table(Qfile,header=F,stringsAsFactors = F)
+colnames(qfile)=c(paste0("Anc", 1:K))
+#add individual ID labels from .fam file
+fam<-read.table(famfile,header=F,stringsAsFactors = F)
+colnames(fam)=c("FID","IID","Mot_ID","Fat_ID","Sex","Pheno")
+qfile$IID<-fam$IID
+#melt the data.frame - i.e. convert to long format
+mqfile<-melt(qfile,id.vars=c("IID"))
+colnames(mqfile)<-c("IID","Ancestry_component","Ancestry")
+#change the order to IID
+mqfile$IID<-factor(mqfile$IID, levels = qfile$IID)
+#mqfile$Ancestry_component <- factor(mqfile$Ancestry_component, levels =c("Anc1","Anc2"))
+
+#plt<-ggplot(mqfile[order(mqfile$IID),],aes(x=factor(IID), y=Ancestry, fill=Ancestry_component))+
+plt<-ggplot(mqfile,aes(x=IID, y=Ancestry, fill=Ancestry_component))+
+  geom_bar(stat="identity",width=1,position="fill")
+#remove individual IDs from x axis as it can get crowded if you have a lot of samples
+plt<-plt+
+  theme(axis.text.x=element_blank(),
+        axis.title.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.line=element_blank(),
+        panel.background=element_blank())+
+  labs(y="Ancestry",fill="Ancestral group") #label axes and legends
+
+#plt
+#one final tweak - change the colors as they are horrendous
+#create new pallete to choose from
+plt<-plt+
+  scale_fill_manual(values=c(
+   "skyblue2","#1b9e77","#7570b3","#d95f02", "khaki2","#FB9A99", # lt pink
+  "gray70", "yellow3",
+  "maroon", "steelblue4",
+   "yellow4", "darkturquoise",
+  "darkorange4",
+  "palegreen2","#6A3D9A", # purple
+  "#FF7F00", # orange
+  "gold1", "deeppink1", "blue1",
+  "#CAB2D6", # lt purple
+  "#FDBF6F", # lt orange
+"dodgerblue2", "#E31A1C", # red
+  "green4"))
+
+# save plot as pdf
+ggsave(plt,file=paste0(Name,"_K",K,"_admixture.pdf"))
